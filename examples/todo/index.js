1a459cf8e8f98f2b6a866f0fe778fc4e58317566
Adeola Uthman <authman2@gmail.com>
2019-03-07 18:09:38 -0500

    Updating the data works, but it's not updating the dom

--- examples/todo/index.js
+++ examples/todo/index.js
@@ -2,23 +2,16 @@ import Mosaic from '../../src/index';
 
 /* Example of a Todo application using Mosaic. */
 
-const Item = new Mosaic({
-    view: function() {
-        return html`<div>
-            <h1>This works!!!</h1>
-            <p>Wow this is awesome!</p>
-        </div>`;
-    }
-})
-
 const TodoItem = new Mosaic({
     created: function() {
-        console.log('Created the TodoItem!');
+        setTimeout(() => {
+            this.data.title = this.data.title + " Now a different title!";
+            console.log('Updated: ', this);
+        }, 5000);
     },
     view: function() {
         return html`<div class='todo-item' onclick="${this.data.deleteTodo}">
             <p>${ this.data.title }</p>
-            ${ Item.new() }
         </div>`
     }
 });
