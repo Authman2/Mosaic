1ad8c19573b37406d77a1d7a28fff680ed353675
Adeola Uthman <authman2@gmail.com>
2019-03-30 20:14:07 -0400

    Update the router to fix a reference error

--- examples/routing/index.js
+++ examples/routing/index.js
@@ -1,18 +1,13 @@
-import Mosaic from '../../dist/index';
+import Mosaic from '../../src/index';
 import { Home, About, Contact } from './routes';
 
 // 1.) Initialize the router.
 const router = new Mosaic.Router('#root');
 
-// 2.) Set the router property on each component you want to use as a page.
-Home.router = router;
-About.router = router;
-Contact.router = router;
-
-// 3.) Add some routes!
+// 2.) Add some routes!
 router.addRoute('/', Home);
 router.addRoute(['/about', '/test'], About);
 router.addRoute('/contact', Contact);
 
-// 4.) Paint the router.
+// 3.) Paint the router.
 router.paint();
