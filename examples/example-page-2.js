da2c9293a8b8476b482952d4e6d1387168df74e6
Adeola Uthman <authman2@gmail.com>
2019-08-11 14:13:51 -0400

    update version with faster array patcher that finds modifications

--- examples/example-page-2.js
+++ examples/example-page-2.js
@@ -12,50 +12,22 @@ export default new Mosaic({
         let str = String.fromCharCode(code);
 
         // Addition.
-        // this.data.letters.push({
-        //     letter: str,
-        //     key: randomKey()
-        // });
-        const start = performance.now();
-        const n = new Array(10000);
-        for(let i = 0; i < 10000; i++)
-            n[i] = {
-                letter: str,
-                key: randomKey()
-            }
-        this.data.letters = n;
-        const end = performance.now();
-        console.log(end - start);
+        this.data.letters.push({
+            letter: str,
+            key: randomKey()
+        });
         code += 1;
     },
     removeSecondAndThird() {
         // Deletion.
-        // this.data.letters.splice(1, 2);
-        const start = performance.now();
         this.data.letters = this.data.letters.filter((_, idx) => idx !== 1 && idx !== 2);
-        const end = performance.now();
-        console.log(end - start);
     },
     modifySecond() {
         // Modification.
-        const start = performance.now();
-        // this.data.letters[1] = {
-        //     letter: 'x',
-        //     key: randomKey()
-        // }
-        const n = new Array(1000);
-        for(let i = 0; i < 1000; i++)
-            n[i] = {
-                letter: 'b',
-                key: randomKey()
-            }
-        this.data.letters = this.data.letters.concat(n);
-        // this.data.letters = [...this.data.letters.slice(0, 10), {
-        //     letter: 'c',
-        //     key: randomKey()
-        // }, ...this.data.letters.slice(10)];
-        const end = performance.now();
-        console.log(end - start);
+        this.data.letters[1] = {
+            letter: 'x',
+            key: randomKey()
+        }
     },
     view() {
         return html`
@@ -74,11 +46,8 @@ export default new Mosaic({
             
             <table>
                 <tbody>
-                    ${Mosaic.list(this.data.letters, obj => obj.key, (obj, index) => {
-                        return html`<tr onclick='${() => console.log(obj)}'>
-                            <td>${index}</td>
-                            <td>${obj.letter}</td>
-                        </tr>`
+                    ${Mosaic.list(this.data.letters, obj => obj.key, obj => {
+                        return html`<h3>${obj.letter}</td></h3>`
                     })}
                 </tbody>
             </table>
